9481f5c80af129b22a2ed797be9edc33dfd8c8ff
Alexander Davidson Bryan <alxdb@gmail.com>
2019-02-04 21:42:13 +0000

    Circular liquids

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import json
+import random
 
 recipe_data = json.load(open('data/recipes.json'))
 recipe_variant = 'normal'
 
-
-class Item:
-    def __init__(self, name, recipes):
-        self.name = name
-        self.recipes = recipes
-
-
-class Recipe:
-    def __init__(self, name, results, energy, dependencies):
-        self.name = name
-        self.results = results
-        self.energy = energy
-        self.dependencies = dependencies
+# class Item:
+#     def __init__(self, name, recipes):
+#         self.name = name
+#         self.recipes = recipes
 
 
-class Dependency:
-    def __init__(self, item, amount):
-        self.item = item
-        self.amount = amount
+# class Recipe:
+#     def __init__(self, name, results, energy, dependencies):
+#         self.name = name
+#         self.results = results
+#         self.energy = energy
+#         self.dependencies = dependencies
 
 
-recipes = set()
+# class Dependency:
+#     def __init__(self, item, amount):
+#         self.item = item
+#         self.amount = amount
+
+
+recipes = []
 for recipe_name in recipe_data:
     recipe = recipe_data[recipe_name]
 
@@ -44,40 +44,44 @@ for recipe_name in recipe_data:
     else:
         energy = 0.5
 
-    recipes.add(Recipe(recipe_name, results, energy, recipe['ingredients']))
+    recipes.append({'Type': 'Recipe',
+                    'name': recipe_name,
+                    'results': results,
+                    'energy': energy,
+                    'dependencies': recipe['ingredients']})
 
 items = dict()
 for recipe in recipes:
-    for result in recipe.results:
+    for result in recipe['results']:
         if result['name'] not in items.keys():
-            items[result['name']] = Item(result['name'], [recipe])
+            items[result['name']] = {
+                'Type': 'Item',
+                'name': result['name'],
+                'recipes': [recipe]}
         else:
             item = items[result['name']]
-            item.recipes.append(recipe)
+            item['recipes'].append(recipe)
 
 for item in items.values():
-    for recipe in item.recipes:
-        for index, dependency in enumerate(recipe.dependencies):
-            if isinstance(dependency, dict):
-                recipe.dependencies[index] = Dependency(
-                    items.get(dependency['name'], dependency['name']), dependency['amount'])
+    for recipe in item['recipes']:
+        for index, dependency in enumerate(recipe['dependencies']):
+            if isinstance(dependency, dict) and dependency.get('Type') is None:
+                recipe['dependencies'][index] = {'Type': 'Dependency',
+                                                 'item': items.get(dependency['name'],
+                                                                   dependency['name']),
+                                                 'amount': dependency['amount']}
             elif isinstance(dependency, list):
-                recipe.dependencies[index] = Dependency(
-                    items.get(dependency[0], dependency[0]), dependency[1])
+                recipe['dependencies'][index] = {'Type': 'Dependency',
+                                                 'item': items.get(dependency[0], dependency[0]),
+                                                 'amount': dependency[1]}
 
-        for result in results:
-            result = {'item': items.get(
-                result['name'], result['name']), 'amount': result['amount']}
 
-example = list(items.values())[0]
-
-# broken
-def itterate(obj, level=0):
-    if isinstance(obj, str):
-        return
-    for entry in dir(obj):
-        if not entry.startswith("__"):
-            print('\t' * level + entry)
-            itterate(entry, level + 1)
-
-itterate(example)
+# fluids cause circular dependency
+try:
+    # item = random.choice(list(items.keys()))
+    item = 'tank'
+    f = open("res.json", "w+")
+    json.dump(items.get(item), f, indent=4)
+except ValueError as err:
+    print(err)
+    print("probably contains liquid: " + item)
